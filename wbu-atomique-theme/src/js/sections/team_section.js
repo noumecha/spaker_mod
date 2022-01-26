b8996eb9e73a28337d40441dd0df9153a1194e72
noumecha <noumechaivan@gmail.com>
2022-01-26 12:05:48 +0100

    maj : tmc-team-section

--- wbu-atomique-theme/src/js/sections/team_section.js
+++ wbu-atomique-theme/src/js/sections/team_section.js
@@ -1,3 +1,2 @@
-import "@stephane888/wbu-atomique/scss/atome/typography/_default.scss";
 import "@stephane888/wbu-atomique/scss/wbu-ressources-clean.scss";
 import "@stephane888/wbu-atomique/scss/organisme/rc-web/hero.scss";
