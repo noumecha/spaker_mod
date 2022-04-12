1a0c2bcb4747170f578decf7731a595aa58761f0
noumecha <noumechaivan@gmail.com>
2022-04-12 15:59:06 +0100

    maj tmc -- conflict twig fix +++++++

--- src/Plugin/Layout/Sections/VesperrPriceSection.php
+++ src/Plugin/Layout/Sections/VesperrPriceSection.php
@@ -17,11 +17,83 @@ use Drupal\formatage_models\Plugin\Layout\FormatageModels;
  *  library = "spaker_mod/vesperr_price",
  *  default_region = "main",
  *  regions = {
- *     "logo" = {
- *       "label" = @Translation("logo"),
+ *     "title" = {
+ *       "label" = @Translation("title"),
  *     },
- *     "link" = {
- *       "label" = @Translation("link"),
+ *     "text" = {
+ *       "label" = @Translation("text"),
+ *     },
+ *     "title_one" = {
+ *       "label" = @Translation("title_one"),
+ *     },
+ *     "title_two" = {
+ *       "label" = @Translation("title_two"),
+ *     },
+ *     "title_three" = {
+ *       "label" = @Translation("title_three"),
+ *     },
+ *     "subtitle_one" = {
+ *       "label" = @Translation("subtitle_one"),
+ *     },
+ *     "subtitle_two" = {
+ *       "label" = @Translation("subtitle_two"),
+ *     },
+ *     "subtitle_three" = {
+ *       "label" = @Translation("subtitle_three"),
+ *     },
+ *     "link_one" = {
+ *       "label" = @Translation("link_one"),
+ *     },
+ *     "link_two" = {
+ *       "label" = @Translation("link_two"),
+ *     },
+ *     "link_three" = {
+ *       "label" = @Translation("link_three"),
+ *     },
+ *     "list_one" = {
+ *       "label" = @Translation("list_one"),
+ *     },
+ *     "list_two" = {
+ *       "label" = @Translation("list_two"),
+ *     },
+ *     "list_three" = {
+ *       "label" = @Translation("list_three"),
+ *     },
+ *     "list_four" = {
+ *       "label" = @Translation("list_four"),
+ *     },
+ *     "list_five" = {
+ *       "label" = @Translation("list_five"),
+ *     },
+ *     "list_six" = {
+ *       "label" = @Translation("list_six"),
+ *     },
+ *     "list_seven" = {
+ *       "label" = @Translation("list_seven"),
+ *     },
+ *     "list_height" = {
+ *       "label" = @Translation("list_height"),
+ *     },
+ *     "list_nine" = {
+ *       "label" = @Translation("list_nine"),
+ *     },
+ *     "list_ten" = {
+ *       "label" = @Translation("list_ten"),
+ *     },
+ *     "list_eleven" = {
+ *       "label" = @Translation("list_eleven"),
+ *     },
+ *     "list_twelve" = {
+ *       "label" = @Translation("list_twelve"),
+ *     },
+ *     "list_thirthteen" = {
+ *       "label" = @Translation("list_thirthteen"),
+ *     },
+ *     "list_fourthteen" = {
+ *       "label" = @Translation("list_fourthteen"),
+ *     },
+ *     "list_fifteen" = {
+ *       "label" = @Translation("list_fifteen"),
  *     },
  *  }
  * )
@@ -63,28 +135,199 @@ class VesperrPriceSection extends FormatageModels
     {
         return parent::defaultConfiguration() + [
 
-            'tmc' => [
+            'titre' => [
                 'builder-form' => true,
                 'info' => [
-                    'title' => 'Contenu 1',
+                    'title' => 'Contenu titre',
                     'loader' => 'static'
                 ],
                 'fields' => [
-                    'link' => [
+                    'title' => [
                         'text_html' => [
-                            'label' => 'lien',
-                            'value' => 'Home'
+                            'label' => 'Titre',
+                            'value' => 'Pricing'
                         ]
                     ],
-                    'logo' => [
+                    'text' => [
                         'text_html' => [
-                            'label' => 'logo',
-                            'value' => 'Wb Universe'
+                            'label' => 'Paragraphe par défaut',
+                            'value' => 'Sit sint consectetur velit nemo qui impedit suscipit alias ea'
+                        ]
+                    ],
+                    'link_one' => [
+                        'text_html' => [
+                            'label' => 'lien 1',
+                            'value' => 'Buy Now'
+                        ]
+                    ],
+                    'link_two' => [
+                        'text_html' => [
+                            'label' => 'lien 2',
+                            'value' => 'Buy Now'
+                        ]
+                    ],
+                    'link_three' => [
+                        'text_html' => [
+                            'label' => 'lien 3',
+                            'value' => 'Buy Now'
+                        ]
+                    ],
+                ]
+            ],
+            'card_one' => [
+                'builder-form' => true,
+                'info' => [
+                    'title' => 'Price card 1',
+                    'loader' => 'static'
+                ],
+                'fields' => [
+                    'title_one' => [
+                        'text_html' => [
+                            'label' => 'titre 1',
+                            'value' => 'Free'
+                        ]
+                    ],
+                    'subtitle_one' => [
+                        'text_html' => [
+                            'label' => 'sous titre 1',
+                            'value' => '<sup>$</sup>0<span> / month</span>'
+                        ]
+                    ],
+                    'list_one' => [
+                        'text_html' => [
+                            'label' => 'elément 1',
+                            'value' => 'Aida dere'
+                        ]
+                    ],
+                    'list_two' => [
+                        'text_html' => [
+                            'label' => 'elément 2',
+                            'value' => 'Nec feugiat nisl'
+                        ]
+                    ],
+                    'list_three' => [
+                        'text_html' => [
+                            'label' => 'elément 3',
+                            'value' => 'Nulla at volutpat dola'
+                        ]
+                    ],
+                    'list_four' => [
+                        'text_html' => [
+                            'label' => 'elément 4',
+                            'value' => 'Pharetra massa'
+                        ]
+                    ],
+                    'list_five' => [
+                        'text_html' => [
+                            'label' => 'elément 5',
+                            'value' => 'Massa ultricies mi'
+                        ]
+                    ],
+                ]
+            ],
+            'card_two' => [
+                'builder-form' => true,
+                'info' => [
+                    'title' => 'Price card two',
+                    'loader' => 'static'
+                ],
+                'fields' => [
+                    'title_two' => [
+                        'text_html' => [
+                            'label' => 'titre 2',
+                            'value' => 'Business'
+                        ]
+                    ],
+                    'subtitle_two' => [
+                        'text_html' => [
+                            'label' => 'sous titre 2',
+                            'value' => '<sup>$</sup>0<span> / month</span>'
+                        ]
+                    ],
+                    'list_six' => [
+                        'text_html' => [
+                            'label' => 'elément 1',
+                            'value' => 'Aida dere'
+                        ]
+                    ],
+                    'list_seven' => [
+                        'text_html' => [
+                            'label' => 'elément 2',
+                            'value' => 'Nec feugiat nisl'
+                        ]
+                    ],
+                    'list_height' => [
+                        'text_html' => [
+                            'label' => 'elément 3',
+                            'value' => 'Nulla at volutpat dola'
+                        ]
+                    ],
+                    'list_nine' => [
+                        'text_html' => [
+                            'label' => 'elément 4',
+                            'value' => 'Pharetra massa'
+                        ]
+                    ],
+                    'list_ten' => [
+                        'text_html' => [
+                            'label' => 'elément 5',
+                            'value' => 'Massa ultricies mi'
+                        ]
+                    ],
+                ]
+            ],
+            'card_three' => [
+                'builder-form' => true,
+                'info' => [
+                    'title' => 'Price card three',
+                    'loader' => 'static'
+                ],
+                'fields' => [
+                    'title_three' => [
+                        'text_html' => [
+                            'label' => 'titre 3',
+                            'value' => 'Developer'
+                        ]
+                    ],
+                    'subtitle_three' => [
+                        'text_html' => [
+                            'label' => 'sous titre 3',
+                            'value' => '<sup>$</sup>29<span> / month</span>'
+                        ]
+                    ],
+                    'list_eleven' => [
+                        'text_html' => [
+                            'label' => 'elément 1',
+                            'value' => 'Aida dere'
+                        ]
+                    ],
+                    'list_twelve' => [
+                        'text_html' => [
+                            'label' => 'elément 2',
+                            'value' => 'Nec feugiat nisl'
+                        ]
+                    ],
+                    'list_thirthteen' => [
+                        'text_html' => [
+                            'label' => 'elément 3',
+                            'value' => 'Nulla at volutpat dola'
+                        ]
+                    ],
+                    'list_fourthteen' => [
+                        'text_html' => [
+                            'label' => 'elément 4',
+                            'value' => 'Pharetra massa'
+                        ]
+                    ],
+                    'list_fifteen' => [
+                        'text_html' => [
+                            'label' => 'elément 5',
+                            'value' => 'Massa ultricies mi'
                         ]
                     ],
                 ]
             ]
 
-                ];
+        ];
     }
 }
