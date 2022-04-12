3785cd1e8d385f5036bdbec731d5e11161795a60
noumecha <noumechaivan@gmail.com>
2022-04-12 13:08:08 +0100

    maj : tmc -- new fix ++

--- src/Plugin/Layout/Sections/VesperrContactSection.php
+++ src/Plugin/Layout/Sections/VesperrContactSection.php
@@ -17,8 +17,29 @@ use Drupal\formatage_models\Plugin\Layout\FormatageModels;
  *  library = "spaker_mod/vesperr_contact",
  *  default_region = "main",
  *  regions = {
- *     "logo" = {
- *       "label" = @Translation("logo"),
+ *     "title" = {
+ *       "label" = @Translation("title"),
+ *     },
+ *     "sub_title" = {
+ *       "label" = @Translation("sub_title"),
+ *     },
+ *     "text_one" = {
+ *       "label" = @Translation("text_one"),
+ *     },
+ *     "text_two" = {
+ *       "label" = @Translation("text_two"),
+ *     },
+ *     "text_three" = {
+ *       "label" = @Translation("text_three"),
+ *     },
+ *     "text_four" = {
+ *       "label" = @Translation("text_four"),
+ *     },
+ *     "text_five" = {
+ *       "label" = @Translation("text_five"),
+ *     },
+ *     "text_six" = {
+ *       "label" = @Translation("text_six"),
  *     },
  *     "link" = {
  *       "label" = @Translation("link"),
@@ -70,16 +91,58 @@ class VesperrContactSection extends FormatageModels
                     'loader' => 'static'
                 ],
                 'fields' => [
+                    'title' => [
+                        'text_html' => [
+                            'label' => 'titre',
+                            'value' => 'Contact Us'
+                        ]
+                    ],
+                    'sub_title' => [
+                        'text_html' => [
+                            'label' => 'sous titre',
+                            'value' => 'Vesperr'
+                        ]
+                    ],
+                    'text_one' => [
+                        'text_html' => [
+                            'label' => 'paragraphe 1',
+                            'value' => 'Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.'
+                        ]
+                    ],
+                    'text_two' => [
+                        'text_html' => [
+                            'label' => 'paragraphe 2',
+                            'value' => 'A108 Adam Street<br>New York, NY 535022'
+                        ]
+                    ],
+                    'text_three' => [
+                        'text_html' => [
+                            'label' => 'paragraphe 3',
+                            'value' => 'info@example.com'
+                        ]
+                    ],
+                    'text_four' => [
+                        'text_html' => [
+                            'label' => 'paragraphe 4',
+                            'value' => '+1 5589 55488 55s'
+                        ]
+                    ],
+                    'text_five' => [
+                        'text_html' => [
+                            'label' => 'texte 1',
+                            'value' => 'Loading'
+                        ]
+                    ],
+                    'text_six' => [
+                        'text_html' => [
+                            'label' => 'texte 2',
+                            'value' => 'Your message has been sent. Thank you!'
+                        ]
+                    ],
                     'link' => [
                         'text_html' => [
                             'label' => 'lien',
-                            'value' => 'Home'
-                        ]
-                    ],
-                    'logo' => [
-                        'text_html' => [
-                            'label' => 'logo',
-                            'value' => 'Wb Universe'
+                            'value' => 'Send Message'
                         ]
                     ],
                 ]
