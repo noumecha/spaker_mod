be1cf46244b340a0761c53bea10f150d493e5bd8
gugledas <azabzistany@gmail.com>
2022-06-14 15:31:01 +0100

    maj

--- wbu-atomique-theme/src/js/sections/vesperr_price.js
+++ wbu-atomique-theme/src/js/sections/vesperr_price.js
@@ -1,1 +1,2 @@
-
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/vesperr/vesperr.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/vesperr/section__price.scss";
