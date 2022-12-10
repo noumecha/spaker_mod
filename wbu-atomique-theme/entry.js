ef9340193811f9470c11c97898b223909cc57792
kouwa stephane <kksasteph888@gmail.com>
2022-12-10 16:17:42 +0100

    Maj

--- wbu-atomique-theme/entry.js
+++ wbu-atomique-theme/entry.js
@@ -1,33 +1,4 @@
 module.exports = {
-  "formatage-models-contact-page":
-    "./src/js/pages/formatage-models-contact-page.js",
-  "formatage-models-isolation-market-contact":
-    "./src/js/pages/formatage-models-isolation-market.js",
-  spaker_mod_home_page: "./src/js/pages/spaker_mod_home_page.js",
-  vesperr_header_tmc: "./src/js/headers/vesperr_header.js",
-  vesperr_hero_tmc: "./src/js/sections/vesperr_hero.js",
-  vesperr_client_tmc: "./src/js/sections/vesperr_client.js",
-  vesperr_about_tmc: "./src/js/sections/vesperr_about.js",
-  vesperr_count_tmc: "./src/js/sections/vesperr_count.js",
-  vesperr_service_tmc: "./src/js/sections/vesperr_services.js",
-  vesperr_more_service: "./src/js/sections/vesperr_more_services.js",
-  vesperr_feature_tmc: "./src/js/sections/vesperr_feature.js",
-  vesperr_testimonial_tmc: "./src/js/sections/vesperr_testimonial.js",
-  vesperr_portfolio_tmc: "./src/js/sections/vesperr_portfolio.js",
-  vesperr_team_tmc: "./src/js/sections/vesperr_team.js",
-  vesperr_price_tmc: "./src/js/sections/vesperr_price.js",
-  vesperr_faq_tmc: "./src/js/sections/vesperr_faq.js",
-  vesperr_contact_tmc: "./src/js/sections/vesperr_contact.js",
-  vesperr_footer_tmc: "./src/js/sections/footers/vesperr_footer.js",
-  team_section_tmc: "./src/js/sections/team_section.js",
-  contact_section_rcweb_tmc: "./src/js/sections/contact_section_rcweb.js",
-  list_section_rcweb_tmc: "./src/js/sections/list_section_rcweb.js",
-  price_section_rcweb_tmc: "./src/js/sections/price_section_rcweb.js",
-  agence_constant_services_tmc: "./src/js/sections/agence_constant_services.js",
-  agence_constant_footer_tmc: "./src/js/sections/agence_constant_footer.js"
+
 };
 
-// export default {
-//   "formatage-models-footer1":
-//     "./src/js/sections/footers/formatage-models-footer1.js",
-// };
