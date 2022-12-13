a976c3a25e2f58dfbf80bf646d0fb33b29cae0bd
noumecha spaker <noumechaivan@gmail.com>
2022-12-13 14:09:15 +0100

    maj : faq teaser && faq section

--- src/Plugin/Layout/Sections/VesperrAskSection.php
+++ src/Plugin/Layout/Sections/VesperrAskSection.php
@@ -16,40 +16,13 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *  path = "layouts/sections",
  *  template = "vesperr_faq",
  *  library = "spaker_mod/vesperr_faq",
- *  default_region = "main",
+ *  default_region = "title",
  *  regions = {
  *     "title" = {
  *       "label" = @Translation("title"),
  *     },
- *     "title_one" = {
- *       "label" = @Translation("title_one"),
- *     },
- *     "title_two" = {
- *       "label" = @Translation("title_two"),
- *     },
- *     "title_three" = {
- *       "label" = @Translation("title_three"),
- *     },
- *     "title_four" = {
- *       "label" = @Translation("title_four"),
- *     },
- *     "title_five" = {
- *       "label" = @Translation("title_five"),
- *     },
- *     "text_one" = {
- *       "label" = @Translation("text_one"),
- *     },
- *     "text_two" = {
- *       "label" = @Translation("text_two"),
- *     },
- *     "text_three" = {
- *       "label" = @Translation("text_three"),
- *     },
- *     "text_four" = {
- *       "label" = @Translation("text_four"),
- *     },
- *     "text_five" = {
- *       "label" = @Translation("text_five"),
+ *     "faq_items_container" = {
+ *       "label" = @Translation("faq_items_container"),
  *     },
  *  }
  * )
@@ -104,64 +77,10 @@ class VesperrAskSection extends FormatageModelsSection
                             'value' => 'Frequently Asked Questions'
                         ]
                     ],
-                    'title_one' => [
+                    'faq_items_container' => [
                         'text_html' => [
                             'label' => 'titre 1',
-                            'value' => 'Non consectetur a erat nam at lectus urna duis?'
-                        ]
-                    ],
-                    'title_two' => [
-                        'text_html' => [
-                            'label' => 'titre 2',
-                            'value' => 'Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?'
-                        ]
-                    ],
-                    'title_three' => [
-                        'text_html' => [
-                            'label' => 'titre 3',
-                            'value' => 'Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?Wb Universe'
-                        ]
-                    ],
-                    'title_four' => [
-                        'text_html' => [
-                            'label' => 'titre 4',
-                            'value' => 'Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?'
-                        ]
-                    ],
-                    'title_five' => [
-                        'text_html' => [
-                            'label' => 'titre 5',
-                            'value' => 'Tempus quam pellentesque nec nam aliquam sem et tortor consequat?'
-                        ]
-                    ],
-                    'text_one' => [
-                        'text_html' => [
-                            'label' => 'texte 1',
-                            'value' => 'Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.Wb Universe'
-                        ]
-                    ],
-                    'text_two' => [
-                        'text_html' => [
-                            'label' => 'texte 2',
-                            'value' => 'Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.'
-                        ]
-                    ],
-                    'text_three' => [
-                        'text_html' => [
-                            'label' => 'texte 3',
-                            'value' => 'Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus.'
-                        ]
-                    ],
-                    'text_four' => [
-                        'text_html' => [
-                            'label' => 'texte 4',
-                            'value' => 'Aperiam itaque sit optio et deleniti eos nihil quidem cumque. Voluptas dolorum accusantium sunt sit enim. Provident consequuntur quam aut reiciendis qui rerum dolorem sit odio. Repellat assumenda soluta sunt pariatur error doloribus fuga.'
-                        ]
-                    ],
-                    'text_five' => [
-                        'text_html' => [
-                            'label' => 'texte 5',
-                            'value' => 'Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in'
+                            'value' => ''
                         ]
                     ],
                 ]
---
+++ src/Plugin/Layout/Teasers/VesperrFaqTeaser.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Drupal\fast_models\Plugin\Layout\Teasers;
+
+use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
+use Drupal\formatage_models\FormatageModelsThemes;
+use Drupal\formatage_models\Plugin\Layout\Teasers\FormatageModelsTeasers;
+
+/**
+ * 
+ * Fast models fn controls teaser 
+ * 
+ * @Layout (
+ *  id = "spaker_mod_faq_teaser",
+ *  label = @Translation("VesperrFaqTeaser"),
+ *  category = @Translation("spaker_mod"),
+ *  path = "layouts/teasers",
+ *  template = "vesperr_faq_teaser",
+ *  library = "spaker_mod/vesperr_faq_teaser",
+ *  default_region = "faq_teaser_svg",
+ *  regions = {
+ *      "faq_teaser_svg" = {
+ *          "label" = @Translation("faq_teaser_svg"),     
+ *      },
+ *      "faq_teaser_title" = {
+ *          "label" = @Translation("faq_teaser_svg"),     
+ *      },
+ *      "faq_teaser_text" = {
+ *          "label" = @Translation("faq_teaser_svg"),     
+ *      },
+ *  }
+ * )
+ * 
+ */
+class VesperrFaqTeaser extends FormatageModelsTeasers
+{
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager)
+    {
+        // TODO Auto-generated method stub
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+        $this->pluginDefinition->set('icon', drupal_get_path('module', 'spaker_mod') . "/icons/teasers/spaker_mod_vesperr_faq_teaser.png");
+    }
+
+    /**
+     * 
+     * {@inheritdoc}
+     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels:build()
+     * 
+     */
+    public function build(array $regions) 
+    {
+        
+        // TODO Auto-generated method stub
+        $build = parent::build($regions);
+        FormatageModelsThemes::formatSettingValues($build);
+        
+        return $build;
+    }
+
+    /**
+     * 
+     * {@inheritdoc}
+     */
+    public function defaultConfiguration()
+    {
+        return parent::defaultConfiguration() + [
+            'css' => '',
+            'fmct' => [
+                'builder-form' => true,
+                'info' => [
+                    'title' => 'Contenu',
+                    'loader' => 'static'
+                ],
+                'fields' => [
+                    'faq_teaser_svg' => [
+                        'text_html' => [
+                            'label' => 'contenu',
+                            'value' => '<svg xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
+                            role="img" width="40" height="40"
+                            preserveaspectratio="xMidYMid meet" viewbox="0 0 24 24">
+                            <path
+                                d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16zm-1-5h2v2h-2v-2zm2-1.645V14h-2v-1.5a1 1 0 0 1 1-1a1.5 1.5 0 1 0-1.471-1.794l-1.962-.393A3.501 3.501 0 1 1 13 13.355z"
+                                fill="#8bc4ea" />
+                            </svg>'
+                        ]
+                    ],
+                    'faq_teaser_title' => [
+                        'text_html' => [
+                            'label' => 'contenu',
+                            'value' => 'Non consectetur a erat nam at lectus urna duis?'
+                        ]
+                    ],
+                    'faq_teaser_text' => [
+                        'text_html' => [
+                            'label' => 'contenu',
+                            'value' => 'Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.Wb Universe'
+                        ]
+                    ],
+                ]
+            ]
+        ];
+    }
+
+}
